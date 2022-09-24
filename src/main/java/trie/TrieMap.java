61b66d995e958df1aaefe0fe0b90de70739570f0
Kanagavelu Sugumar <skanagavelu@users.noreply.github.com>
2022-09-24 14:44:19 +0530

    Update TrieMap.java

--- src/main/java/trie/TrieMap.java
+++ src/main/java/trie/TrieMap.java
@@ -4,9 +4,9 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
-import static trie.BitSet32Util.bitAt;
-import static trie.BitSet32Util.elementCount;
-import static trie.BitSet32Util.nextSetBit;
+import static trie.BitSet16Util.bitAt;
+import static trie.BitSet16Util.elementCount;
+import static trie.BitSet16Util.nextSetBit;
 /**
  * An object that maps key to value.  A map cannot contain duplicate keys;
  * each key can map to at most one value.
@@ -293,22 +293,17 @@ class PartialArrayEdge<K, V> implements Node<K, V> {
     private void clearBitAt(int bitIndex) { bitset &= ~(1 << bitIndex); }
 }
 
-class BitSet32Util {
+class BitSet16Util {
 
-    private static int[] masks32 = {
-
+    private static int[] masks16 = {
             0x1,         0x3,          0x7,         0xF,
             0x1F,        0x3F,         0x7F,        0xFF,
             0x1FF,       0x3FF,        0x7FF,       0xFFF,
-            0x1FFF,      0x3FFF,       0x7FFF,      0xFFFF,
-            0x1FFFF,     0x3FFFF,      0x7FFFF,     0xFFFFF,
-            0x1FFFFF,    0x3FFFFF,     0x7FFFFF,    0xFFFFFF,
-            0x1FFFFFF,   0x3FFFFFF,    0x7FFFFFF,   0xFFFFFFF,
-            0x1FFFFFFF,  0x3FFFFFFF,   0x7FFFFFFF,  0xFFFFFFFF
+            0x1FFF,      0x3FFF,       0x7FFF,      0xFFFF
     };
 
     static int nextSetBit(int bitset, int fromIndex) {
-        int word = bitset & (0xFFFFFFFF << fromIndex);
+        int word = bitset & (0xFFFF << fromIndex); //max only 16 bits were set
         if (word != 0) { return Integer.numberOfTrailingZeros(word); };
         return -1;
     }
@@ -322,7 +317,6 @@ class BitSet32Util {
      * @return
      */
      static int elementCount(int bitset, int firstXBits) {
-
-        return Integer.bitCount(bitset & masks32[firstXBits]);
+        return Integer.bitCount(bitset & masks16[firstXBits]);
     }
 }
